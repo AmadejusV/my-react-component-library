279b48c87ffc0a3162d8aba139d42cdf4bf9c8b5
Amadejus Valionis <Amadejus.Valionis@bdo.global>
2023-06-17 10:56:57 +0300

    Add css support

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,6 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import postcss from "rollup-plugin-postcss";
 import packageJson from "./package.json" assert { type: "json" };
 
 export default [
@@ -21,11 +22,13 @@ export default [
         resolve(),
         commonjs(),
         typescript({ tsconfig: "./tsconfig.json" }),
+        postcss()
     ]
 },
 {
     input: "dist/esm/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "es" }],
-    plugins: [dts()]
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+    external: [/\.css$/]
 }
 ]
